28f8ca066802fef87e174c2739699351276b3c47
Sasi Kathimanda <Sasi.Kathimanda@metapack.com>
2018-01-09 19:43:54 +0000

    adding Supplier example

---
+++ src/main/java/lambda/built/in/SupplierExample.java
@@ -0,0 +1,31 @@
+package lambda.built.in;
+
+import java.util.function.Supplier;
+
+/**
+    A Supplier does the opposite of the <Consumer>,takes no argument and only returns some value.
+ **/
+
+public class SupplierExample {
+    public static void main(String[] args) {
+
+        // using anonymous class
+        String t = "Supplier";
+        Supplier<String> supplierUsingAnonymousClazz =  new Supplier<String>() {
+            @Override
+            public String get(){
+                return t.toLowerCase();
+            }
+        };
+        System.out.println("supplierUsingAnonymousClazz - > "+supplierUsingAnonymousClazz.get());
+
+        //using Lambda method
+
+        Supplier<String> supplierUsingLambda = () -> t.toLowerCase();
+
+        System.out.println("supplierUsingLambda - > "+supplierUsingLambda.get());
+
+    }
+
+
+}
