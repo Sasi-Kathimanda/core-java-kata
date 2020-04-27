2b48fc381f2d62cb8eaedd07fe1f836687e81231
Sasi Kathimanda <Sasi.Kathimanda@metapack.com>
2020-04-27 21:53:32 +0100

    solved Map-2: AllSwap

--- src/main/java/codebat/map2/AllSwap.java
+++ src/main/java/codebat/map2/AllSwap.java
@@ -1,18 +1,35 @@
 package codebat.map2;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 public class AllSwap {
     public static void main(String[] args) {
         String[] list1 = new String[]{"ab", "ac"};
         String[] list2 = new String[]{"ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"};
         String[] list3 = new String[]{"ax", "bx", "ay", "by", "ai", "aj", "bx", "by"};
 
-        System.out.println(allSwap(list1));
+        Stream.of(allSwap(list1)).forEach(System.out::println);
+        Stream.of(allSwap(list2)).forEach(System.out::println);
+        Stream.of(allSwap(list3)).forEach(System.out::println);
 
     }
 
     public static String[] allSwap(String[] strings) {
-//        Arrays.stream(strings).collect(Collectors.toMap(k->k.substring(0,1),v-> strings.))
-        return new String[] {};
+        Map<String, Integer> tempMap =  new HashMap<>();
+            for (int i =0; i < strings.length; i++) {
+                String currentElementFirstCharacter = strings[i].substring(0, 1);
+                if(tempMap.containsKey(currentElementFirstCharacter)) {
+                    String temp = strings[i];
+                    String matched = strings[tempMap.get(currentElementFirstCharacter)];
+                    strings[i] = matched;
+                    strings[tempMap.get(currentElementFirstCharacter)] = temp;
+                    tempMap.remove(currentElementFirstCharacter);
+                } else {
+                    tempMap.put(currentElementFirstCharacter, i);
+                }
+            }
+        return strings;
     }
-
 }
