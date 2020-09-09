743f56936d2ea396d7b90b10a22cd66589ccf87e
Sasi Kathimanda <ksasikiranraja@gmail.com>
2020-09-09 08:01:42 +0100

    adding stream reduce to find sum

--- src/main/java/streams/reducing/StreamReduction.java
+++ src/main/java/streams/reducing/StreamReduction.java
@@ -9,13 +9,20 @@ public class StreamReduction {
     public static void main(String[] args) {
 
         //finding sum using peek
+        System.out.println("finding sum using using peek");
         IntStream intStream = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        System.out.format("\n%d",intStream.peek(s -> System.out.format("%d",s)).sum());
+        System.out.println(intStream.peek(i-> System.out.format("%d",i)).sum());
+
+        // finding sum using Streams sum method
+        System.out.println("finding sum using stream's reduce");
+        intStream = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println(intStream.reduce((i,n) -> i+n).getAsInt());
+
 
         // finding sum using Streams sum method
         System.out.println("finding sum using stream's sum");
-        IntStream intStream2 = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        System.out.println(intStream2.sum());
+        intStream = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println(intStream.sum());
 
 
     }
