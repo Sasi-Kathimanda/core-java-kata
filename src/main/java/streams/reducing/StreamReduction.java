9d8f22238184758b5aa815e08ce477779614cc5a
Sasi Kathimanda <ksasikiranraja@gmail.com>
2020-09-14 07:42:42 +0100

    finding max using streams reduce

--- src/main/java/streams/reducing/StreamReduction.java
+++ src/main/java/streams/reducing/StreamReduction.java
@@ -13,10 +13,18 @@ public class StreamReduction {
         IntStream intStream = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         System.out.println(intStream.peek(i-> System.out.format("%d",i)).sum());
 
-        // finding sum using Streams sum method
+        // finding sum using Streams reduce method
         System.out.println("finding sum using stream's reduce");
         intStream = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         System.out.println(intStream.reduce((i,n) -> i+n).getAsInt());
+
+        //finding max using  streams reduce
+        System.out.println("finding max using stream's reduce");
+        intStream = IntStream.of(1, 2, 3, 41, 5, 6, 7, 8, 9, 10);
+        System.out.println(intStream.reduce((i,n) -> i > n ? i:n));
+        System.out.println(intStream.reduce((i,n) -> Math.max(i, n))); // replacing Math.max
+        System.out.println(intStream.reduce(Math::max)); // replacing Lambda Method Reference
+
 
 
         // finding sum using Streams sum method
