6372b61719124403e3588142182e28632a53412b
Sasi Kathimanda <ksasikiranraja@gmail.com>
2020-10-13 23:05:58 +0100

    fizz buzz and two sum

---
+++ src/main/java/prgramming/challenges/TwoSumTest.java
@@ -0,0 +1,35 @@
+package prgramming.challenges;
+
+import java.util.*;
+
+public class TwoSumTest {
+    public static void main(String[] args) {
+        int[] intArray = new int [] {2,3,7,4,9};
+        printTwoSumViaBruteForce(intArray,10);
+        printTwoSumViaOnePass(intArray, 10);
+    }
+
+    private static void printTwoSumViaBruteForce(int[] intArray, int target) {
+        for(int i = 0 ; i< intArray.length; i++) {
+            for (int j = 0; j < intArray.length;j++) {
+                if(intArray[i]+intArray[j] == target) {
+                    System.out.println(i+" "+j);
+                }
+            }
+        }
+    }
+
+    private static void  printTwoSumViaOnePass(int[] input, int target) {
+        Map<Integer,Integer> recorder = new HashMap<>();
+
+        //add elements to recorder
+        for (int i = 0 ; i < input.length; i++) {
+            recorder.put(input[i],i);
+            int delta = target - input[i];
+            if(recorder.containsKey(delta)) {
+                System.out.println(i+"-" + recorder.get(delta));
+            }
+        }
+        }
+
+}
