34c1d61df74005a7105d1cb68abd4a4fa1b23aa6
Sasi Kathimanda <ksasikiranraja@gmail.com>
2020-10-24 15:58:31 +0100

    adding factorial and different implementations

---
+++ src/main/java/prgramming/challenges/FactorialTest.java
@@ -0,0 +1,36 @@
+package prgramming.challenges;
+
+import java.math.BigInteger;
+
+public class FactorialTest {
+    public static void main(String[] args) {
+        System.out.println(findFactorialForAVeryLargeNumber(100));
+        System.out.println(findFactorialForAVeryLargeNumberSimplified(100));
+        System.out.println(findFactorial(100));
+
+    }
+
+    private static BigInteger findFactorialForAVeryLargeNumber(int input) {
+        BigInteger factorial = BigInteger.ONE ;
+        for(int i = input-1; input > input-i; i--){
+            factorial = factorial.add(factorial.multiply(BigInteger.valueOf(i)));
+        }
+        return factorial;
+    }
+
+    private static int findFactorial(int input) {
+        int factorial =1 ;
+        for(int i = input-1; input > input-i; i--){
+            factorial += factorial*i;
+        }
+        return factorial;
+    }
+
+    private static BigInteger findFactorialForAVeryLargeNumberSimplified(int input) {
+        BigInteger factorial = BigInteger.ONE ;
+        for(int i =2; i <=input; i++){
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        return factorial;
+    }
+}
