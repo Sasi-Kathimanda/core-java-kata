d47aa382cc6f50b1659a8f80708600429b663112
Sasi Kathimanda <ksasikiranraja@gmail.com>
2020-10-29 22:18:21 +0000

    fixing the message

--- src/main/java/prgramming/challenges/codility/NumberOfTrailingZerosFactorialTest.java
+++ src/main/java/prgramming/challenges/codility/NumberOfTrailingZerosFactorialTest.java
@@ -2,17 +2,18 @@ package prgramming.challenges.codility;
 
 public class NumberOfTrailingZerosFactorialTest {
     public static void main(String[] args) {
-        int n = 219;
+        int n = 100;
         countNumberOfTrailingZeros(n);
     }
 
     private static void countNumberOfTrailingZeros(int n) {
         long zeros = 0;
+        int n1 = n;
         while (n != 0) {
             zeros += n / 5;
             n /= 5;
         }
-        System.out.printf("Number of trailing zeros of the factorial %d is %d ", n, zeros);
+        System.out.printf("Number of trailing zeros of the factorial %d is %d ", n1, zeros);
         System.out.printf("\n");
     }
 }
