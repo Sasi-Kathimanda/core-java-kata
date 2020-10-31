c65901aaccaad2282b97ff726bc6f3b4a631b7e7
Sasi Kathimanda <ksasikiranraja@gmail.com>
2020-10-31 23:08:04 +0000

    finishing up the number of zeros in the decimals test

--- src/main/java/prgramming/challenges/codility/NumberOfZerosDecimalTest.java
+++ src/main/java/prgramming/challenges/codility/NumberOfZerosDecimalTest.java
@@ -2,52 +2,24 @@ package prgramming.challenges.codility;
 
 public class NumberOfZerosDecimalTest {
     public static void main(String[] args) {
-      //  System.out.println(countNumberOfZeros(219));
+        System.out.println(countNumberOfZeros("100"));
+    }
+
+    private static int countNumberOfZeros(String input) {
+        return getCount(Integer.parseInt(input));
+    }
+
+    private static int getCount(int n) {
         int count = 0;
-        int n = 100;
         for (int i = 0; i <= n; i++) {
-            int m= i;
+            int n1 = i;
             do {
-                if (m % 10 == 0)
-                    ++count;
-
-                m = m / 10;
-            } while (m > 0);
-        }
-        System.out.println("count = " + count);
-
-    }
-
-    private static int countNumberOfZeros(int input) {
-        return getCount(input);
-    }
-
-    private static int getCount(int input) {
-        int zeroes = 0;
-        for (int i = 0; i <= input; i++) {
-          //  if (hasZero(i) >= 1) {
-                zeroes += hasZero(i);
-        //    }
-        }
-        return zeroes;
-    }
-
-    private static int hasZero(int n) {
-        int count = 0;
-        if (n == 0) {
-            return 1;
-        }
-        while (n != 0) {
-            if (n % 10 == 0) {
-                if (n % 100 == 0) {
-                   // System.out.println(n);
+                if (n1 % 10 == 0) {
                     count++;
-                    return count;
                 }
-                return 1;
-            }
-            n = n / 10;
+                n1 = n1 / 10;
+            } while (n1 != 0);
         }
-        return 0;
+        return count;
     }
 }
