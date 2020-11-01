26af5e55f73a5f4329f5f5cf00612dfd34a027b9
Sasi Kathimanda <ksasikiranraja@gmail.com>
2020-11-01 22:06:04 +0000

    wip for max zero product

--- src/main/java/prgramming/challenges/codility/CyclicRotationTest.java
+++ src/main/java/prgramming/challenges/codility/CyclicRotationTest.java
@@ -1,4 +1,7 @@
 package prgramming.challenges.codility;
 
 public class CyclicRotationTest {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/main/java/prgramming/challenges/codility/MaxZeroProductTest.java
@@ -0,0 +1,15 @@
+package prgramming.challenges.codility;
+
+public class MaxZeroProductTest {
+    public static void main(String[] args) {
+        int[] A = new int[] {7,15,6,20,5,10};
+        findMaxZeroProduct(A);
+    }
+
+    private static void findMaxZeroProduct(int[] input) {
+        for (int i = 0 ; i < input.length; i++) {
+
+        }
+    }
+
+}
