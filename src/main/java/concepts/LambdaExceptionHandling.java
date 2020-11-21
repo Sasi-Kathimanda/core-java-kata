c12d742f74e7cbdde82688abbd1c5bb406290fa2
Sasi Kathimanda <ksasikiranraja@gmail.com>
2020-11-21 12:50:25 +0000

    adding handler for unchecked exception

--- src/main/java/concepts/LambdaExceptionHandling.java
+++ src/main/java/concepts/LambdaExceptionHandling.java
@@ -1,14 +1,33 @@
 package concepts;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LambdaExceptionHandling {
   public static void main(String[] args) {
-    // Handling checked Exception
+    // Handling checked exception
     Stream<Integer> ints = Stream.of(1, 2, 3, 4, 5);
-    ints.map(LambdaExceptionHandling::sleep)
-        .collect(Collectors.toList());
+    System.out.println(
+        " = " + ints.map(LambdaExceptionHandling::sleep).collect(Collectors.toList()));
+
+    // handling unchecked exception
+    List<Integer> intList = Arrays.asList(1, 2, 3, 4);
+    int key = 0;
+    process(intList, key, (k, v) -> System.out.println("k/v = " + k / v));
+  }
+
+  private static void process(
+      List<Integer> intList, int key, BiConsumer<Integer, Integer> consumer) {
+    for (int i : intList) {
+      try {
+        consumer.accept(i, key);
+      } catch (ArithmeticException ae) {
+        System.out.println("Arithmetic Exception Thrown");
+      }
+    }
   }
 
   private static Integer sleep(Integer t) {
