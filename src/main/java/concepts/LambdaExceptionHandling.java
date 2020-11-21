5230d98d05955692af5a23dcd53787e7c8eac4e7
Sasi Kathimanda <ksasikiranraja@gmail.com>
2020-11-21 12:14:41 +0000

    extracting to method reference

--- src/main/java/concepts/LambdaExceptionHandling.java
+++ src/main/java/concepts/LambdaExceptionHandling.java
@@ -7,14 +7,16 @@ public class LambdaExceptionHandling {
   public static void main(String[] args) {
     // Handling checked Exception
     Stream<Integer> ints = Stream.of(1, 2, 3, 4, 5);
-    ints.map(t-> {
-          try {
-            Thread.sleep(1);
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-          return t+1;
+    ints.map(LambdaExceptionHandling::sleep)
+        .collect(Collectors.toList());
+  }
+
+  private static Integer sleep(Integer t) {
+    try {
+      Thread.sleep(1);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
-    ).collect(Collectors.toList());
+    return t + 1;
   }
 }
