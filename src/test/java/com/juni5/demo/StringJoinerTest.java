c16b5478a77c67bb0555eb3c2786a7f6ed05a83b
Sasi Kathimanda <ksasikiranraja@gmail.com>
2021-02-26 08:14:24 +0000

    more test cases for string joiner

--- src/test/java/com/juni5/demo/StringJoinerTest.java
+++ src/test/java/com/juni5/demo/StringJoinerTest.java
@@ -1,12 +1,14 @@
 package com.juni5.demo;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.StringJoiner;
 
-class StringJoinerTest  {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StringJoinerTest {
     @Test
     @DisplayName("stringJoiner_When_Mandatory_Delimiter_Supplied")
     void stringJoinerWhenMandatoryDelimiterSupplied() {
@@ -14,7 +16,57 @@ class StringJoinerTest  {
         sj.add("sasi");
         sj.add("kiran");
         sj.add("raja");
-        Assertions.assertEquals("sasi-kiran-raja",sj.toString());
-        Assertions.assertEquals("sasi-kiran-raja",String.format("%s-%s-%s","sasi","kiran","raja"));
+        assertEquals("sasi-kiran-raja", sj.toString());
+        assertEquals("sasi-kiran-raja", String.format("%s-%s-%s", "sasi", "kiran", "raja"));
+    }
+
+    @Test
+    @DisplayName("joiner_when_using_a_List")
+    void joinerWhenUsingAList() {
+        StringJoiner sj = new StringJoiner("-");
+        List<String> list = List.of("Sasi", "Kiran", "Raja");
+        for (String s : list) {
+            sj.add(s);
+        }
+        assertEquals("Sasi-Kiran-Raja", sj.toString());
+    }
+
+    @Test
+    @DisplayName("joiner_when_the_feeder_isEmpty")
+    void joinerWhenTheFeederIsEmpty() {
+        StringJoiner joiner = new StringJoiner("-");
+        assertEquals(0, joiner.toString().length());
+    }
+
+    @Test
+    @DisplayName("joiner_when_the_feeder_isEmpty_And_There_Is_Default")
+    void joinerWhenTheFeederIsEmpty_And_There_Is_Default() {
+        StringJoiner joiner = new StringJoiner("-");
+        joiner.setEmptyValue("DEFAULT");
+        assertEquals("DEFAULT", joiner.toString());
+    }
+
+    @Test
+    @DisplayName("joiner_when_there_is_Prefix_Suffix_Applied")
+    void joinerWhenThereIsPrefixSuffixApplied() {
+        StringJoiner joiner = new StringJoiner("-", "{", "}");
+        joiner.add("Sasi");joiner.add("Kathimanda");
+        assertEquals("{Sasi-Kathimanda}",joiner.toString());
+    }
+
+
+    @Test
+    @DisplayName("joiner_when_there_is_Prefix_Suffix_And_No_Feeder")
+    void joinerWhenThereIsPrefixSuffixAndNoFeeder() {
+        StringJoiner joiner = new StringJoiner("-", "{", "}");
+        assertEquals("{}",joiner.toString());
+    }
+
+    @Test
+    @DisplayName("joiner_when_there_is_Prefix_Suffix_And_No_Feeder_And_Have_Default")
+    void joinerWhenThereIsPrefixSuffixAndNoFeederAndHaveDefaultSet() {
+        StringJoiner joiner = new StringJoiner("-", "{", "}");
+        joiner.setEmptyValue("default");
+        assertEquals("default",joiner.toString());
     }
 }
