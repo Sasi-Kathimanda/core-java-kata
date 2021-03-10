9ef0d82cf1961dcfbbfe593e0805c2741d86328b
Sasi Kathimanda <ksasikiranraja@gmail.com>
2021-03-10 22:08:21 +0000

    adding test to find the max of the ints list

--- src/test/java/com/junit5/demo/StreamReduceTests.java
+++ src/test/java/com/junit5/demo/StreamReduceTests.java
@@ -3,6 +3,7 @@ package com.junit5.demo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,5 +15,10 @@ class StreamReduceTests {
         var intStream = IntStream.of(1,2,3,4,5,6,7,8,9,10);
         assertEquals(55,intStream.reduce((i,n) -> i+n).getAsInt());
     }
-
+    @Test
+    @DisplayName("GivenInts_ThenFindMaxUsingReduce")
+    void givenIntsThenFindMaxUsingReduce() {
+        var intList = List.of(45,28,63,56,3,9);
+        assertEquals(63,intList.stream().reduce((i,n) -> i > n? i:n).get());
+    }
 }
