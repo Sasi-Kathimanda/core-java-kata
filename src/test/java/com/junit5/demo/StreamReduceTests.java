438405f26721ff737f4bcabc1a62ce7c94093b7b
Sasi Kathimanda <ksasikiranraja@gmail.com>
2021-03-11 08:13:40 +0000

    optmised imports

--- src/test/java/com/junit5/demo/StreamReduceTests.java
+++ src/test/java/com/junit5/demo/StreamReduceTests.java
@@ -5,15 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StreamReduceTests {
     @Test
