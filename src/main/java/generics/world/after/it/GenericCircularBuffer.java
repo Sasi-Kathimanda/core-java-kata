6d3ded051bc14df6e68f65d2303dc0f585ab7959
Sasi Kathimanda <ksasikiranraja@gmail.com>
2021-11-11 21:49:16 +0000

    [GENERICS] - generify the circular buffer

---
+++ src/main/java/generics/world/after/it/GenericCircularBuffer.java
@@ -0,0 +1,33 @@
+package generics.world.after.it;
+
+public class  GenericCircularBuffer<T> {
+    private  T[] buffer;
+    private int readCursor = 0;
+    private int writeCursor = 0;
+
+    public GenericCircularBuffer(int size) {
+        buffer = (T[]) new Object[size];
+    }
+
+    public boolean offer(T value) {
+        if (buffer[writeCursor] != null) {
+            return false;
+        }
+        buffer[writeCursor] = value;
+        writeCursor = next(writeCursor);
+        return true;
+    }
+
+    public T poll() {
+        var value = buffer[readCursor];
+        if (buffer[readCursor] != null) {
+            buffer[readCursor] = null;
+            readCursor = next(readCursor);
+        }
+        return value;
+    }
+
+    private int next(int index) {
+        return (index + 1) % buffer.length;
+    }
+}
