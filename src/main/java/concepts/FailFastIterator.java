b4fdce2d79a5bcb78086c2c44e410f80144567c7
Sasi Kathimanda <ksasikiranraja@gmail.com>
2021-11-13 23:56:16 +0000

    formatting

--- src/main/java/concepts/FailFastIterator.java
+++ src/main/java/concepts/FailFastIterator.java
@@ -4,13 +4,14 @@ import java.util.*;
 import java.util.stream.*;
 
 public class FailFastIterator {
-  public static void main(String[] args) {
-    List<Integer> list = Stream.of(1, 2, 3, 4, 5).collect(Collectors.toList());
-    Iterator<Integer> listIterator = list.iterator();
-    while (listIterator.hasNext()) {
-      Integer currentVal = listIterator.next(); // throws the ConcurrentModificationException after 1st iteration as it checks the initial modCount
-      System.out.println("currentVal = " + currentVal);
-      list.add(6);
+    public static void main(String[] args) {
+        List<Integer> list = Stream.of(1, 2, 3, 4, 5)
+                .collect(Collectors.toList());
+        Iterator<Integer> listIterator = list.iterator();
+        while (listIterator.hasNext()) {
+            Integer currentVal = listIterator.next(); // throws the ConcurrentModificationException after 1st iteration as it checks the initial modCount
+            System.out.println("currentVal = " + currentVal);
+            list.add(6);
+        }
     }
-  }
 }
--- src/main/java/concepts/LambdaExceptionHandling.java
+++ src/main/java/concepts/LambdaExceptionHandling.java
@@ -7,34 +7,35 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LambdaExceptionHandling {
-  public static void main(String[] args) {
-    // Handling checked exception
-    Stream<Integer> ints = Stream.of(1, 2, 3, 4, 5);
-    System.out.println(ints.map(LambdaExceptionHandling::sleep).collect(Collectors.toList()));
+    public static void main(String[] args) {
 
-    // handling unchecked exception
-    List<Integer> intList = Arrays.asList(1, 2, 3, 4);
-    int key = 2;
-    process(intList, key, (k, v) -> System.out.println("k/v = " + k / v));
-  }
+        // Handling checked exception
+        Stream<Integer> ints = Stream.of(1, 2, 3, 4, 5);
+        System.out.println(ints.map(LambdaExceptionHandling::sleep).collect(Collectors.toList()));
 
-  private static void process(
-      List<Integer> intList, int key, BiConsumer<Integer, Integer> consumer) {
-    for (int i : intList) {
-      try {
-        consumer.accept(i, key);
-      } catch (ArithmeticException ae) {
-        System.out.println("Arithmetic Exception Thrown");
-      }
+        // handling unchecked exception
+        List<Integer> intList = Arrays.asList(1, 2, 3, 4);
+        int key = 2;
+        process(intList, key, (k, v) -> System.out.println("k/v = " + k / v));
     }
-  }
 
-  private static Integer sleep(Integer t) {
-    try {
-      Thread.sleep(1);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
+    private static void process(
+            List<Integer> intList, int key, BiConsumer<Integer, Integer> consumer) {
+        for (int i : intList) {
+            try {
+                consumer.accept(i, key);
+            } catch (ArithmeticException ae) {
+                System.out.println("Arithmetic Exception Thrown");
+            }
+        }
     }
-    return t + 1;
-  }
+
+    private static Integer sleep(Integer t) {
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return t + 1;
+    }
 }
