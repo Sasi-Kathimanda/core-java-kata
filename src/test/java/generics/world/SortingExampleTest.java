e0841145810949688badd2258f175ca1adae9560
Sasi Kathimanda <ksasikiranraja@gmail.com>
2021-11-14 20:39:29 +0000

    [GENERICS] - adding more tests for sorting by age

--- src/test/java/generics/world/SortingExampleTest.java
+++ src/test/java/generics/world/SortingExampleTest.java
@@ -6,16 +6,29 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SortingExampleTest {
 
+    List<Person> people = List.of(new Person("Sasi", 26), new Person("Kiran", 37), new Person("Raja", 19));
+    List<Person> sortedPeopleByAge = List.of(new Person("Raja", 19), new Person("Sasi", 26), new Person("Kiran", 37));
+
     @Test
-    void sortByAgeShouldSortListByAgeAscending() {
-        List<Person> people = List.of(new Person("Sasi", 26), new Person("Kiran", 37), new Person("Raja", 19));
-        List<Person> sortedPeopleByAge = List.of(new Person("Raja", 19), new Person("Sasi", 26), new Person("Kiran", 37));
+    void sortByAgeShouldSortListByAgeAscendingForUnorderedList() {
 
         List<Person> expectedPeople = new SortingExample().sortByAge(people);
         assertEquals(3, expectedPeople.size());
-        assertEquals(sortedPeopleByAge, expectedPeople);
+        assertEquals(expectedPeople, sortedPeopleByAge);
     }
+
+    @Test
+    void shouldSortByAgeReturnSameGivenAlreadySortedList() {
+        assertEquals(sortedPeopleByAge, new SortingExample().sortByAge(sortedPeopleByAge));
+    }
+
+    @Test
+    void shouldReturnZeroListWhenInputIsEmpty() {
+        assertEquals(0, new SortingExample().sortByAge(List.of()).size());
+    }
+
 }
