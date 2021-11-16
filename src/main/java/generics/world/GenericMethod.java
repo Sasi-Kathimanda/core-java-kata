cd0429cce34f07917a495c4a94a004c301a36715
Sasi Kathimanda <ksasikiranraja@gmail.com>
2021-11-16 22:32:56 +0000

    [GENERICS] - adding docs for generic method min

--- src/main/java/generics/world/GenericMethod.java
+++ src/main/java/generics/world/GenericMethod.java
@@ -4,7 +4,13 @@ import java.util.Comparator;
 import java.util.List;
 
 public class GenericMethod {
-
+    /**
+     *
+     * @param objects list of any objects
+     * @param comparator any T that implements comparator interface
+     * @param <T> any type T
+     * @return
+     */
     public <T> T min(List<T> objects, Comparator<T> comparator) {
         if (objects.isEmpty()) {
             throw new IllegalArgumentException("list is empty ");
