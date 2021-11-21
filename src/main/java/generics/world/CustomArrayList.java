32bbd5035b5facd607a023301bb86de94352a690
Sasi Kathimanda <ksasikiranraja@gmail.com>
2021-11-21 21:20:51 +0000

    [GENERICS] - adding the implementation for custom array list and tests passing

--- src/main/java/generics/world/CustomArrayList.java
+++ src/main/java/generics/world/CustomArrayList.java
@@ -3,18 +3,30 @@ package generics.world;
 import java.util.AbstractList;
 
 public class CustomArrayList extends AbstractList {
+
+    private final Object[] values;
+
+    public CustomArrayList(Object[] values) {
+        this.values = values;
+    }
+
     @Override
     public Object get(int index) {
-        return null;
+        return values[index];
     }
 
     @Override
     public int size() {
-        return 0;
+        return values.length;
     }
 
     @Override
     public boolean add(Object o) {
+        Object[] newValues = new Object[size() + 1];
+        for (int i = 0; i < values.length; i++) {
+                newValues[i] = values[i];
+        }
+        newValues[size()] = o;
         return true;
     }
 }
--- src/test/java/generics/world/CustomArrayListTest.java
+++ src/test/java/generics/world/CustomArrayListTest.java
@@ -1,16 +1,19 @@
 package generics.world;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CustomArrayListTest {
-
-    private final CustomArrayList arrayList = new CustomArrayList();
-
     @Test
     void shouldStoreTheElements() {
+        Integer[] ints = new Integer[] {1,2};
+        CustomArrayList arrayList = new CustomArrayList(ints);
         arrayList.add(1);
         arrayList.add(2);
-        Assertions.assertEquals(2, arrayList.size());
+        assertEquals(2, arrayList.size());
+        assertEquals(1,arrayList.get(0));
+        assertEquals(2,arrayList.get(1));
+
     }
 }
