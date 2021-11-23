bd0a48f60fa80f17adfc2e6f39c361d6c3755a77
Sasi Kathimanda <ksasikiranraja@gmail.com>
2021-11-23 20:48:46 +0000

    [REFACTOR] - refactor conditional lambda to have unit test rather sop

--- src/main/java/lambda/ConditionalLambda.java
+++ src/main/java/lambda/ConditionalLambda.java
@@ -6,21 +6,7 @@ import java.util.List;
 
 public class ConditionalLambda {
 
-    public static void main(String[] args) {
-        List<Person> persons =
-            Arrays.asList(
-                new Person("person1", 18),
-                new Person("person2", 76),
-                new Person("person3", 23),
-                new Person("person4", 12));
-
-        List<Person> personsWithAgeGT75 = findPersons(persons, person -> person.getAge() > 75);
-        List<Person> personsWithAgeGT18 = findPersons(persons, person -> person.getAge() > 18);
-        System.out.println(personsWithAgeGT75.stream().peek(s -> System.out.println("**" + s.name)).count());
-        System.out.println(personsWithAgeGT18.stream().peek(s -> System.out.println("**" + s.name)).count());
-    }
-
-    private static List<Person> findPersons(List<Person> persons, Searchable searchable) {
+    public List<Person> findPersons(List<Person> persons, Searchable searchable) {
         List<Person> searchedPersons = new ArrayList<>();
         for (Person person : persons) {
             if (searchable.test(person)) {
---
+++ src/test/java/lambda/ConditionalLambdaTest.java
@@ -0,0 +1,22 @@
+package lambda;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionalLambdaTest {
+   private final  List<Person> persons = List.of(
+            new Person("person1",18),
+            new Person("person2",76),
+            new Person("person3",23),
+            new Person("person4",12));
+
+   @Test
+    void givenListOfIntFindPersonsCountWithAgeGreaterThanANumber() {
+       assertEquals(1, new ConditionalLambda().findPersons(persons, persons -> persons.getAge() > 75).size());
+       assertEquals(3, new ConditionalLambda().findPersons(persons, persons -> persons.getAge() > 12).size());
+   }
+
+}
