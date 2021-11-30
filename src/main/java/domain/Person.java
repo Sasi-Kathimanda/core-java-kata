aa89078e84379f1a27e6408ac7e48f3f07845931
Sasi Kathimanda <ksasikiranraja@gmail.com>
2021-11-30 23:57:16 +0000

    declutter person domain using lombok

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -1,43 +1,11 @@
 package domain;
 
-import java.util.Objects;
+import lombok.Data;
 
+
+@Data
 public class Person {
-    private final String name;
-    private final int age;
+    private  String name;
+    private  int age;
 
-    public Person(String name, int age) {
-        Objects.requireNonNull(name);
-        this.name = name;
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return age == person.age && Objects.equals(name, person.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, age);
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
