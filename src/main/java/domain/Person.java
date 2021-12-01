3516c708c0a74b2887c5f608248cdbb7b55c587a
Sasi Kathimanda <ksasikiranraja@gmail.com>
2021-12-01 00:22:57 +0000

    fixing lombok compile time issues

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -1,11 +1,13 @@
 package domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@AllArgsConstructor
 public class Person {
     private  String name;
     private  int age;
-
 }
