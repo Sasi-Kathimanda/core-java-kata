b8a4ecc82f911d0ab89caf1c8e16a556ebcf7317
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2022-02-26 22:05:58 +0000

    add the test for converting different types to lists

--- src/test/java/streams/collect/StreamsCollectTest.java
+++ src/test/java/streams/collect/StreamsCollectTest.java
@@ -1,12 +1,25 @@
 package streams.collect;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StreamsCollectTest {
+    private StreamsCollect sut;
 
     @BeforeEach
     void setUp() {
+        sut = new StreamsCollect();
+    }
+
+    @Test
+    void convertDifferentTypesToLists() {
+        List<Integer> ints = sut.convertFromTypeToList(1, 2, 3, 4, 5);
+        List<String> strings = sut.convertFromTypeToList("Sasi", "Kiran", "Aashi", "Aaru");
+        assertEquals("[1, 2, 3, 4, 5]", ints.toString());
+        assertEquals("[Sasi, Kiran, Aashi, Aaru]",strings.toString());
     }
 }
