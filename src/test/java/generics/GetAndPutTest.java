014c0afec6e3ce1b18f2913614d2f3e485f4eca3
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2022-03-02 21:37:42 +0000

    [Generics] - adding test to pass list of double to wildcard extends

--- src/test/java/generics/GetAndPutTest.java
+++ src/test/java/generics/GetAndPutTest.java
@@ -12,8 +12,13 @@ class GetAndPutTest {
     @Test
     void getTheValueOutOfStructure() {
         GetAndPut sut = new GetAndPut();
+        //Can give List<Integer>
         double actual = sut.sum(List.of(1, 2, 3, 4, 5));
         assertEquals(15.0, actual);
+
+        //Can give List<Double>
+        actual = sut.sum(List.of(1.0, 2.0, 3.0, 4.1, 5));
+        assertEquals(15.1, actual);
     }
 
 }
