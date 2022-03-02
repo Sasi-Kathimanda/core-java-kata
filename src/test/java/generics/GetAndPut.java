ff0ec9c3644a3cc39e24b1d0177499b427fa47a9
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2022-03-02 21:35:18 +0000

    [Generics]- adding implementation for sum where arguments have wildcard extends

--- src/test/java/generics/GetAndPut.java
+++ src/test/java/generics/GetAndPut.java
@@ -5,6 +5,10 @@ import java.util.Collection;
 public class GetAndPut {
 
     public double sum(Collection<? extends Number> nums) {
-        return 0.0;
+        double temp = 0.0;
+        for (Number integer : nums) {
+            temp +=integer.doubleValue();
+        }
+        return temp;
     }
 }
