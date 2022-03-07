7cdfe5737570d5a9b5502326422c729f5d030c7d
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2022-03-07 21:02:42 +0000

    [generics] - correcting contravariance definition

--- src/main/java/generics/VariantTypes.java
+++ src/main/java/generics/VariantTypes.java
@@ -6,7 +6,7 @@ import java.util.List;
  *  Three variant types
  *  1) Covariant - S[] is considered to be subtype of T[] whenever S is a subtype of T eg: Arrays Subtyping
  *  2) Invariant - List<S> is not considered to be subtype of List<T> except S ang T are identical. eg: Subtyping in Generics follows this
- *  3) Contravariant - List<S> is considered to be subtype of List<? extends T> when S is a super type of T
+ *  3) Contravariant - List<S> is considered to be subtype of List<? super T> when S is a super type of T
  */
 
 public class VariantTypes {
@@ -18,8 +18,11 @@ public class VariantTypes {
 
     public void InvariantTest(List<Integer> ints) {
       //  List<Number> nums = ints; //WARNING Compiler error you cannot assign as List<Integer> is not a subtype List<Number>
-          List<Integer> ints2 = ints; //This is allowes as both types are identical
-    };
+         // List<Integer> ints2 = ints; //This is allows as both types are identical
+    }
 
+    public void ContraVariantTest() {
+
+    }
 
 }
