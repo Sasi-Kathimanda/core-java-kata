d70c7d0bd55cebdc1f0da413cb46be22c4a23911
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2022-03-16 22:05:48 +0000

    [DateTime]-adding failing test for converting the string to java instant

--- src/main/java/time/formatter/JavaTimeFormatter.java
+++ src/main/java/time/formatter/JavaTimeFormatter.java
@@ -1,10 +1,15 @@
 package time.formatter;
 
 
+import java.time.Instant;
 import java.time.LocalDate;
 
 public class JavaTimeFormatter {
     protected LocalDate convertToDate(String date) {
         return LocalDate.parse(date);
+    }
+
+    public Instant convertToInstant(String s) {
+        return Instant.now();
     }
 }
--- src/test/java/time/formatter/JavaTimeFormatterTest.java
+++ src/test/java/time/formatter/JavaTimeFormatterTest.java
@@ -3,6 +3,7 @@ package time.formatter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
 import java.util.Locale;
@@ -19,9 +20,16 @@ class JavaTimeFormatterTest {
     void convertStringToDate() {
         JavaTimeFormatter sut = new JavaTimeFormatter();
         LocalDate localDate = sut.convertToDate("1984-12-02");
-        assertEquals("1984-12-02",localDate.toString());
-        assertEquals("December",localDate.getMonth().getDisplayName(TextStyle.FULL, Locale.UK));
-        assertEquals("SUNDAY",localDate.getDayOfWeek().name());
-        assertEquals(1984,localDate.getYear());
+        assertEquals("1984-12-02", localDate.toString());
+        assertEquals("December", localDate.getMonth().getDisplayName(TextStyle.FULL, Locale.UK));
+        assertEquals("SUNDAY", localDate.getDayOfWeek().name());
+        assertEquals(1984, localDate.getYear());
+    }
+
+    @Test
+    void convertStringToInstant() {
+        JavaTimeFormatter sut = new JavaTimeFormatter();
+        Instant actual = sut.convertToInstant("1984-08-13T10:15:30.345Z");
+        assertEquals(1647468090, actual.getEpochSecond());
     }
 }
