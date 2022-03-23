89d5c9276414af11285e4883af2f918e6eb810f7
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2022-03-23 20:46:56 +0000

    [DateTime]- add test for ISO time passing localdate and local time

--- src/test/java/time/formatter/JavaDateTimeFormatterTest.java
+++ src/test/java/time/formatter/JavaDateTimeFormatterTest.java
@@ -4,28 +4,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JavaDateTimeFormatterTest {
-    JavaDateTimeFormatter dateTimeFormatter;
+    private static final LocalDate LOCAL_DATE = LocalDate.of(1984, 12, 2);
+    JavaDateTimeFormatter sut;
 
     @BeforeEach
     void setUp() {
-        dateTimeFormatter = new JavaDateTimeFormatter();
+        sut = new JavaDateTimeFormatter();
     }
 
     @Test
     void testInbuiltFormatterForDates() {
-        var actual = dateTimeFormatter.formatter(DateTimeFormatter.BASIC_ISO_DATE, LocalDate.of(1984, 12, 2));
+        var actual = sut.formatter(DateTimeFormatter.BASIC_ISO_DATE, LOCAL_DATE);
         assertEquals("19841202", actual);
 
-        actual = dateTimeFormatter.formatter(DateTimeFormatter.ISO_DATE, LocalDate.of(1984, 12, 2));
+        actual = sut.formatter(DateTimeFormatter.ISO_DATE, LOCAL_DATE);
         assertEquals("1984-12-02", actual);
 
-        actual = dateTimeFormatter.formatter(DateTimeFormatter.ISO_LOCAL_DATE, LocalDate.of(1984, 12, 2));
+        actual = sut.formatter(DateTimeFormatter.ISO_LOCAL_DATE, LOCAL_DATE);
         assertEquals("1984-12-02", actual);
 
+    }
+
+    @Test
+    void testInbuiltFormatterForTime() {
+        var actual = sut.formatter(DateTimeFormatter.ISO_TIME, LocalDateTime.of(LOCAL_DATE, LocalTime.of(10,15,30)));
+        assertEquals("10:15:30", actual);
     }
 }
