8851ad866d818b61d5fbd3ac8b1f580c06476fe7
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2022-03-28 20:32:06 +0100

    [OOAD]-adding helper method to intiate inventory

--- src/test/java/hf/ooad/InventoryTest.java
+++ src/test/java/hf/ooad/InventoryTest.java
@@ -14,15 +14,15 @@ class InventoryTest {
     @Test
     void testAddMobileToInventory() {
         Inventory inventory = new Inventory();
-        Mobile m1 = Mobile.builder()
-                .mobileNo(9440964913L)
-                .brand("Apple")
-                .network("Jio")
-                .price(999)
-                .simFree(true)
-                .build();
-
+        Mobile m1 = Mobile.builder().mobileNo(9440964913L).brand("Apple").network("Jio").price(999).simFree(true).build();
         inventory.addMobile(m1);
         assertEquals("Apple",inventory.getMobileList().get(0).getBrand());
+    }
+
+    private static void initializeInventory (Inventory inventory) {
+        inventory.addMobile(Mobile.builder().mobileNo(9440964913L).brand("Apple").network("Jio").price(999).simFree(true).build());
+        inventory.addMobile(Mobile.builder().mobileNo(9491279678L).brand("Apple").network("Jio").price(999).simFree(true).build());
+        inventory.addMobile(Mobile.builder().mobileNo(74405195886L).brand("Samsung").network("Sky").price(399).simFree(true).build());
+        inventory.addMobile(Mobile.builder().mobileNo(7511577533L).brand("Apple").network("o2").price(999).simFree(true).build());
     }
 }
