646ecc9b3cebf48945cc3f3742fe72f94ceaa8d5
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2022-03-28 20:51:44 +0100

    [OOAD]- failing test for the mobile search functionality

--- src/test/java/hf/ooad/InventoryTest.java
+++ src/test/java/hf/ooad/InventoryTest.java
@@ -6,23 +6,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryTest {
+    Inventory sut;
+    Mobile m1 = Mobile.builder().mobileNo(7511577533L).brand("Apple").modelName("13 Pro").network("o2").price(999).simFree(true).build();
 
     @BeforeEach
     void setUp() {
+        sut = new Inventory();
     }
 
     @Test
     void testAddMobileToInventory() {
-        Inventory inventory = new Inventory();
         Mobile m1 = Mobile.builder().mobileNo(9440964913L).brand("Apple").network("Jio").price(999).simFree(true).build();
-        inventory.addMobile(m1);
-        assertEquals("Apple",inventory.getMobileList().get(0).getBrand());
+        sut.addMobile(m1);
+        assertEquals("Apple", sut.getMobileList().get(0).getBrand());
     }
 
-    private static void initializeInventory (Inventory inventory) {
-        inventory.addMobile(Mobile.builder().mobileNo(9440964913L).brand("Apple").network("Jio").price(999).simFree(true).build());
-        inventory.addMobile(Mobile.builder().mobileNo(9491279678L).brand("Apple").network("Jio").price(999).simFree(true).build());
-        inventory.addMobile(Mobile.builder().mobileNo(74405195886L).brand("Samsung").network("Sky").price(399).simFree(true).build());
-        inventory.addMobile(Mobile.builder().mobileNo(7511577533L).brand("Apple").network("o2").price(999).simFree(true).build());
+    @Test
+    void testSearchMobile() {
+        initializeInventory(sut);
+        Mobile searchResults = sut.search(m1);
+        assertEquals(m1, searchResults);
+
+    }
+
+    private static void initializeInventory(Inventory inventory) {
+        inventory.addMobile(Mobile.builder().mobileNo(9440964913L).brand("Apple").modelName("X").network("Jio").price(999).simFree(true).build());
+        inventory.addMobile(Mobile.builder().mobileNo(9491279678L).brand("Apple").modelName("11").network("Jio").price(999).simFree(true).build());
+        inventory.addMobile(Mobile.builder().mobileNo(74405195886L).brand("Samsung").modelName("Oneplus").network("Sky").price(399).simFree(true).build());
+        inventory.addMobile(Mobile.builder().mobileNo(7511577533L).brand("Apple").modelName("13 Pro").network("o2").price(999).simFree(true).build());
     }
 }
