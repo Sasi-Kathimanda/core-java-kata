45ed3036e51cff39a3c41c02cbcbdd98d83d490d
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-04-03 21:41:49 +0100

    [DateTime]-move parser class under time package

--- src/main/java/time/JavaTimeParser.java
+++ src/main/java/time/JavaTimeParser.java
@@ -1,4 +1,4 @@
-package time.formatter;
+package time;
 
 
 import java.time.Instant;
--- src/test/java/time/JavaTimeParserTest.java
+++ src/test/java/time/JavaTimeParserTest.java
@@ -1,4 +1,4 @@
-package time.formatter;
+package time;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
