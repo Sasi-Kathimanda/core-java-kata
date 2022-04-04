d3e8d953d75c3ce796bdaec5dcd8f78e92ab1fd5
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2022-04-04 21:24:21 +0100

    [DateTime]- add failing test for duraiton betweeen

--- src/main/java/time/JavaDuration.java
+++ src/main/java/time/JavaDuration.java
@@ -1,11 +1,16 @@
 package time;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.time.temporal.TemporalUnit;
 
 public class JavaDuration {
 
     protected Duration of(long lon, TemporalUnit temporalUnit) {
         return Duration.of(lon, temporalUnit);
+    }
+
+    public Duration between(Instant instant1, Instant instant2) {
+        return Duration.ofDays(30L);
     }
 }
--- src/test/java/time/JavaDurationTest.java
+++ src/test/java/time/JavaDurationTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JavaDurationTest {
     JavaDuration sut;
@@ -19,6 +22,14 @@ class JavaDurationTest {
     @Test
     void durationOf() {
         Duration duration = sut.of(30, ChronoUnit.DAYS);
-        Assertions.assertEquals(30, duration.toDays());
+        assertEquals(30, duration.toDays());
+    }
+
+    @Test
+    void durationBetween() {
+        Instant instant1 = Instant.parse("1984-12-02T10:15:30.345Z");
+        Instant instant2 = Instant.parse("2022-04-04T21:18:20.1845Z");
+        Duration duration = sut.between(instant1,instant2);
+        assertEquals(36,duration.toDays()/12);
     }
 }
