a8a6cf1cc2d892b3006f615e2c1a4d7edacd784a
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-05-27 22:55:01 +0100

    [BAU]- remvoving wildcards for the import

--- src/test/java/streams/reducing/StreamReductionTest.java
+++ src/test/java/streams/reducing/StreamReductionTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class StreamReductionTest {
     private final List<Integer> ints = List.of(1, 2, 3, 4, 5);
