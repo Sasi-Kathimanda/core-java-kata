8da6f96893407cdedb3f4a075490097bf6acdf1f
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-06-15 22:22:44 +0100

    [BAU]- tidying up the wildcard imports

--- src/test/java/generics/VariantTypesTest.java
+++ src/test/java/generics/VariantTypesTest.java
@@ -2,7 +2,7 @@ package generics;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class VariantTypesTest {
 
