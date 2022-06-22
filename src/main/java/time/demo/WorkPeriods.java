e89390111899698ae141c71b70f9e98b9bd10388
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-06-22 22:01:28 +0100

    [DATETIME]-adding failing test for working periods

--- src/main/java/time/demo/WorkPeriods.java
+++ src/main/java/time/demo/WorkPeriods.java
@@ -1,14 +1,18 @@
 package time.demo;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class WorkPeriods {
 
     private WorkPeriods() {
     }
 
-    public static WorkPeriod createWorkPeriod(LocalDateTime start) {
+    public static WorkPeriod createFullWorkPeriod(LocalDateTime start) {
         LocalDateTime end = LocalDateTime.from(start).plusHours(8).plusMinutes(0);
         return new WorkPeriod(start, end);
+    }
+    public static List<WorkPeriod> createWorkingPeriods(LocalDateTime start, int dayCount) {
+        return List.of();
     }
 }
--- src/test/java/time/demo/WorkPeriodsTest.java
+++ src/test/java/time/demo/WorkPeriodsTest.java
@@ -5,19 +5,26 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 class WorkPeriodsTest {
+    public static final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(2022, 6, 21, 9, 5);
     private WorkPeriods sut;
 
     @BeforeEach
     void setUp() {
     }
 
-
     @Test
-    void createWorkPeriod() {
-        WorkPeriod actual = WorkPeriods.createWorkPeriod(LocalDateTime.of(2022, 6, 21, 9, 5));
+    void createFullWorkPeriod() {
+        WorkPeriod actual = WorkPeriods.createFullWorkPeriod(LOCAL_DATE_TIME);
         Assertions.assertEquals("2022-06-21T09:05", actual.getStartTime().toString());
         Assertions.assertEquals("2022-06-21T17:05", actual.getEndTime().toString());
+    }
+
+    @Test
+    void createWorkingPeriods() {
+        List<WorkPeriod> workPeriods = WorkPeriods.createWorkingPeriods(LOCAL_DATE_TIME, 2);
+        Assertions.assertEquals(2, workPeriods.size());
     }
 }
