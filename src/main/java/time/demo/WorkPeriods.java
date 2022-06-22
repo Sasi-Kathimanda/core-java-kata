70fb7362ac248b0aec7fe687a197bc0522cf5783
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-06-22 22:46:05 +0100

    [DATETIME]-adding passing test for creation of working periods

--- src/main/java/time/demo/WorkPeriods.java
+++ src/main/java/time/demo/WorkPeriods.java
@@ -1,7 +1,11 @@
 package time.demo;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WorkPeriods {
 
@@ -12,7 +16,22 @@ public class WorkPeriods {
         LocalDateTime end = LocalDateTime.from(start).plusHours(8).plusMinutes(0);
         return new WorkPeriod(start, end);
     }
-    public static List<WorkPeriod> createWorkingPeriods(LocalDateTime start, int dayCount) {
-        return List.of();
+
+    public static List<WorkPeriod> createWorkingPeriods(LocalDate start, int dayCount) {
+        List<LocalDate> workingDays = generateWorkingDays(start, dayCount);
+        return workingDays.stream()
+                .map(d -> createFullWorkPeriod(d.atTime(8,0)))
+                .collect(Collectors.toList());
+    }
+
+    private static List<LocalDate> generateWorkingDays(LocalDate start, int dayCount) {
+        return Stream.iterate(start, d -> d.plusDays(1))
+                .filter(WorkPeriods::isWorkingDay)
+                .limit(dayCount)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isWorkingDay(LocalDate localDate) {
+        return !List.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY).contains(localDate.getDayOfWeek());
     }
 }
--- src/test/java/time/demo/WorkPeriodsTest.java
+++ src/test/java/time/demo/WorkPeriodsTest.java
@@ -1,14 +1,17 @@
 package time.demo;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class WorkPeriodsTest {
     public static final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(2022, 6, 21, 9, 5);
+    public static final LocalDate LOCAL_DATE = LocalDate.of(2022, 6, 21);
     private WorkPeriods sut;
 
     @BeforeEach
@@ -18,13 +21,15 @@ class WorkPeriodsTest {
     @Test
     void createFullWorkPeriod() {
         WorkPeriod actual = WorkPeriods.createFullWorkPeriod(LOCAL_DATE_TIME);
-        Assertions.assertEquals("2022-06-21T09:05", actual.getStartTime().toString());
-        Assertions.assertEquals("2022-06-21T17:05", actual.getEndTime().toString());
+        assertEquals("2022-06-21T09:05", actual.getStartTime().toString());
+        assertEquals("2022-06-21T17:05", actual.getEndTime().toString());
     }
 
     @Test
     void createWorkingPeriods() {
-        List<WorkPeriod> workPeriods = WorkPeriods.createWorkingPeriods(LOCAL_DATE_TIME, 2);
-        Assertions.assertEquals(2, workPeriods.size());
+        List<WorkPeriod> workPeriods = WorkPeriods.createWorkingPeriods(LOCAL_DATE, 2);
+        assertEquals(2, workPeriods.size());
+        assertEquals(21, workPeriods.get(0).getStartTime().getDayOfMonth());
+        assertEquals(22, workPeriods.get(1).getStartTime().getDayOfMonth());
     }
 }
