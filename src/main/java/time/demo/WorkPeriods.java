92827f9ed9f443f8a454169aed2d3aa1be93f2a2
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-06-23 23:12:33 +0100

    [DATETIME]-adding the am pm work periods

--- src/main/java/time/demo/WorkPeriods.java
+++ src/main/java/time/demo/WorkPeriods.java
@@ -1,13 +1,20 @@
 package time.demo;
 
 import java.time.DayOfWeek;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class WorkPeriods {
+
+    public static final LocalTime MORNING_AM_START = LocalTime.of(8, 5);
+    public static final LocalTime EVENING_PM_START = LocalTime.of(12, 5);
+    public static final Duration AM_DURATION_HOURS = Duration.ofHours(3).plusMinutes(30);
+    public static final Duration PM_DURATION_HOURS = Duration.ofHours(4).plusMinutes(30);
 
     private WorkPeriods() {
     }
@@ -17,10 +24,29 @@ public class WorkPeriods {
         return new WorkPeriod(start, end);
     }
 
+    public static WorkPeriod createMorningWorkPeriod(LocalDate start) {
+        LocalDateTime startTime = LocalDateTime.of(start, MORNING_AM_START);
+        LocalDateTime endTime = startTime.plus(AM_DURATION_HOURS);
+        return new WorkPeriod(startTime, endTime);
+    }
+
+    public static WorkPeriod createEveningWorkPeriod(LocalDate start) {
+        LocalDateTime startTime = LocalDateTime.of(start, EVENING_PM_START);
+        LocalDateTime endTime = startTime.plus(PM_DURATION_HOURS);
+        return new WorkPeriod(startTime, endTime);
+    }
+
     public static List<WorkPeriod> createWorkingPeriods(LocalDate start, int dayCount) {
         List<LocalDate> workingDays = generateWorkingDays(start, dayCount);
         return workingDays.stream()
-                .map(d -> createFullWorkPeriod(d.atTime(8,0)))
+                .map(d -> createFullWorkPeriod(d.atTime(8, 0)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<WorkPeriod> createWorkingPeriodsForAM_AND_PM(LocalDate start, int dayCount) {
+        List<LocalDate> workingDays = generateWorkingDays(start, dayCount);
+        return workingDays.stream()
+                .flatMap(d -> Stream.of(createMorningWorkPeriod(d), createMorningWorkPeriod(d)))
                 .collect(Collectors.toList());
     }
 
