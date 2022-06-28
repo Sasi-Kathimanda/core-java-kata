4302cb6f463f2be9a6d4344bebfeb75621400393
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-06-28 21:18:27 +0100

    [DATETIME]-adding birthday date time in india

--- src/test/java/time/JavaInstantTest.java
+++ src/test/java/time/JavaInstantTest.java
@@ -1,13 +1,14 @@
 package time;
 
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JavaInstantTest {
     public static final String INSTANCE_EPOCH = "1970-01-01T00:00:00Z";
@@ -20,20 +21,26 @@ class JavaInstantTest {
 
     @Test
     void instanceOf() {
-        Assertions.assertEquals(INSTANCE_EPOCH, sut.Of().toString());
+        assertEquals(INSTANCE_EPOCH, sut.Of().toString());
     }
 
     @Test
     void instantToZonedDuration() {
         var time = Instant.ofEpochSecond(Duration.ofDays(115).plusHours(6).plusMinutes(59).plusSeconds(59).toSeconds());
-        Assertions.assertEquals("1970-04-26T06:59:59Z", time.toString());
+        assertEquals("1970-04-26T06:59:59Z", time.toString());
 
         var zonedTime = time.atZone(ZoneId.of("America/New_York"));
-        Assertions.assertEquals("1970-04-26T01:59:59-05:00[America/New_York]", zonedTime.toString());
+        assertEquals("1970-04-26T01:59:59-05:00[America/New_York]", zonedTime.toString());
 
         //add ONE second
         time = time.plusSeconds(1);
         zonedTime = time.atZone(ZoneId.of("America/New_York"));
-        Assertions.assertEquals("1970-04-26T03:00-04:00[America/New_York]", zonedTime.toString());
+        assertEquals("1970-04-26T03:00-04:00[America/New_York]", zonedTime.toString());
+    }
+
+    @Test
+    void testBirthdayInDifferentZones() {
+        var utc = Instant.ofEpochSecond(470811618).atZone(ZoneId.of("Asia/Kolkata"));
+        assertEquals("1984-12-02T10:30:18+05:30[Asia/Kolkata]", utc.toString());
     }
 }
