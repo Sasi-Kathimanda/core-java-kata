40b10d446ddece1bc6503130a9670ba5e8c2bf2b
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-06-28 21:01:10 +0100

    [DATETIME]-adding test for converting the instant to zonedDateTime

--- src/test/java/time/JavaInstantTest.java
+++ src/test/java/time/JavaInstantTest.java
@@ -5,7 +5,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+
 class JavaInstantTest {
+    public static final String INSTANCE_EPOCH = "1970-01-01T00:00:00Z";
     private JavaInstant sut;
 
     @BeforeEach
@@ -15,7 +20,15 @@ class JavaInstantTest {
 
     @Test
     void instanceOf() {
-        Assertions.assertEquals("1970-01-01T00:00:00Z", sut.Of().toString());
+        Assertions.assertEquals(INSTANCE_EPOCH, sut.Of().toString());
     }
 
+    @Test
+    void instantToZonedDuration() {
+        var time = Instant.ofEpochSecond(Duration.ofDays(115).plusHours(6).plusMinutes(59).plusSeconds(59).toSeconds());
+        Assertions.assertEquals("1970-04-26T06:59:59Z", time.toString());
+
+        var zonedTime = time.atZone(ZoneId.of("America/New_York"));
+        Assertions.assertEquals("1970-04-26T01:59:59-05:00[America/New_York]", zonedTime.toString());
+    }
 }
