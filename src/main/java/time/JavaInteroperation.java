50ebba474566b1fa1a5528c53f13923bffc7d815
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-07-04 22:12:07 +0100

    [DATETIME]-adding test for calendar to instant conversion

--- src/main/java/time/JavaInteroperation.java
+++ src/main/java/time/JavaInteroperation.java
@@ -1,6 +1,7 @@
 package time;
 
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 
 public class JavaInteroperation {
@@ -9,7 +10,11 @@ public class JavaInteroperation {
         return date.toInstant();
     }
 
-    public Date dateFromInstant(Instant epochSeconds) {
+    protected Date dateFromInstant(Instant epochSeconds) {
         return Date.from(epochSeconds);
+    }
+
+    protected Instant calendarToInstant(Calendar calendar) {
+        return calendar.toInstant();
     }
 }
--- src/test/java/time/JavaInteroperationTest.java
+++ src/test/java/time/JavaInteroperationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -23,7 +24,7 @@ class JavaInteroperationTest {
     void shouldGetInstantFromLegacyDate() throws ParseException {
         //Given
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        format.setTimeZone(TimeZone.getTimeZone("GMT"));
+        format.setTimeZone(TimeZone.getTimeZone("UTC"));
         var date = format.parse("1970-01-01T00:00:00Z");
         //When
         var expectedInstant = date.toInstant();
@@ -41,5 +42,19 @@ class JavaInteroperationTest {
         //When
         var actualDate = sut.dateFromInstant(epochSeconds);
         assertEquals(expectedDate, actualDate);
+    }
+
+    @Test
+    void shouldGetInstantFromCalendar() {
+        //Given
+        var calendar = new Calendar.Builder()
+                .setDate(1984, 11, 2)
+                .setTimeOfDay(5, 0, 18, 0)
+                .setTimeZone(TimeZone.getTimeZone("UTC"))
+                .build();
+
+        //When
+        var actualInstant = sut.calendarToInstant(calendar);
+        assertEquals(Instant.ofEpochMilli(470_811_618_000L), actualInstant);
     }
 }
