12294394cfd349796beea1dcde7571d7e00b0633
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-07-11 21:48:56 +0100

    [DATETIME]-add offset date time test to be generated from local date and local time

---
+++ src/main/java/time/JavaOffSetDateTime.java
@@ -0,0 +1,11 @@
+package time;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+public class JavaOffSetDateTime {
+    protected static OffsetDateTime of(LocalDateTime dateTime, ZoneOffset zof) {
+        return OffsetDateTime.MAX;
+    }
+}
---
+++ src/test/java/time/JavaOffSetDateTimeTest.java
@@ -0,0 +1,22 @@
+package time;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+
+class JavaOffSetDateTimeTest {
+
+    @Test
+    void givenDateTimeGetOffsetDateTime() {
+        var dateTime = LocalDateTime.of(
+                LocalDate.of(1984, 12, 2),
+                LocalTime.of(5, 15));
+
+        var offSetTime = JavaOffSetDateTime.of(dateTime, ZoneOffset.of("+05:30"));
+        Assertions.assertEquals("1984-12-02T05:15:00:000+05:30", offSetTime.toString());
+    }
+}
