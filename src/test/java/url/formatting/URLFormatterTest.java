ae2e01800baa9c7fe6e485519fa2a35594b418fb
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-08-10 07:13:55 +0100

    [URL-FORMATTING]- writing test for the encoding url only for the query

--- src/test/java/url/formatting/URLFormatterTest.java
+++ src/test/java/url/formatting/URLFormatterTest.java
@@ -1,22 +1,38 @@
 package url.formatting;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class URLFormatterTest {
+    String url;
+
+    @BeforeEach
+    void setUp() {
+        url = "https://www.google.co.uk/search?q=test";
+    }
+
     @Test
     void urlSchemaDesign() throws MalformedURLException {
-        String url = "https://www.google.co.uk/search?q=test";
-
         URL urlFormatter = new URL(url);
         assertEquals("www.google.co.uk", urlFormatter.getHost());
         assertEquals("https", urlFormatter.getProtocol());
         assertEquals("q=test", urlFormatter.getQuery());
     }
 
+    @Test
+    void givenPlainUrlEncodeIt() throws URISyntaxException {
+        //only encode the query part
+        var encodedUrl = URLEncoder.encode(new URI(url).getQuery(), StandardCharsets.UTF_8);
+        assertEquals("q%3Dtest", encodedUrl);
+    }
 }
