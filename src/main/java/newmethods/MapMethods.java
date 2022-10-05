5ebe49c7b12185a14b607f454bc9cc7e2bc28137
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-10-05 21:05:41 +0100

    [CORE] - simplyfying map declaration

--- src/main/java/newmethods/MapMethods.java
+++ src/main/java/newmethods/MapMethods.java
@@ -2,30 +2,30 @@ package newmethods;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import lambda.Person;
 
 public class MapMethods {
 
     public static void main(String[] args) {
-        Map<String, Map<String, Person>> map = new HashMap<String, Map<String, Person>>() { {
-            putIfAbsent("one", new HashMap<String, Person>() {{
-                 putIfAbsent("one",new Person("sasi",19));
-                }});
-        }
-        };
+        Map<String, Map<String, Person>> map = new HashMap<>();
+        Map<String, Person> subMap = new HashMap<>();
+        subMap.put("one", new Person("sasi", 19));
+        map.put("one", subMap);
+
         printMap(map);
         Person computedMap = map.computeIfAbsent(
-            "one", key -> new HashMap<String, Person>()
-                                                ).put("two", new Person("Dan", 29));
-        System.out.println("computedMap="+computedMap);
+                "one", key -> new HashMap<String, Person>()
+        ).put("two", new Person("Dan", 29));
+        System.out.println("computedMap=" + computedMap);
         printMap(map);
 
     }
 
     private static void printMap(Map<String, Map<String, Person>> map) {
         System.out.println("--------------------------------------------------------------------------");
-        map.forEach( (k,v) -> {
-            System.out.println("key="+ k +":value=" +v.entrySet() );
+        map.forEach((k, v) -> {
+            System.out.println("key=" + k + ":value=" + v.entrySet());
         });
         System.out.println("--------------------------------------------------------------------------");
     }
