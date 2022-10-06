267c914a15a7f11f2726fb77486099c050918755
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-10-06 22:21:45 +0100

    [MAP]- cleaning up and simplyfying computeIf absent

--- src/main/java/newmethods/MapMethods.java
+++ src/main/java/newmethods/MapMethods.java
@@ -1,32 +1,14 @@
 package newmethods;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
-import lambda.Person;
+public class MapMethods<K, V> {
 
-public class MapMethods {
+    // default V computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction)
+    public V computeIfAbsent(Map<K, V> map, K key, Function<? super K, ? extends V> mappingFunction) {
 
-    public static void main(String[] args) {
-        Map<String, Map<String, Person>> map = new HashMap<>();
-        Map<String, Person> subMap = new HashMap<>();
-        subMap.put("one", new Person("sasi", 19));
-        map.put("one", subMap);
-
-        printMap(map);
-        Person computedMap = map.computeIfAbsent(
-                "one", key -> new HashMap<String, Person>()
-        ).put("two", new Person("Dan", 29));
-        System.out.println("computedMap=" + computedMap);
-        printMap(map);
-
+        return map.computeIfAbsent(key, mappingFunction);
     }
 
-    private static void printMap(Map<String, Map<String, Person>> map) {
-        System.out.println("--------------------------------------------------------------------------");
-        map.forEach((k, v) -> {
-            System.out.println("key=" + k + ":value=" + v.entrySet());
-        });
-        System.out.println("--------------------------------------------------------------------------");
-    }
 }
