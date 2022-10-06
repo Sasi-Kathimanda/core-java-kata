3885112b7b6d9e97bf6b5fd6b63eaea53aa2302d
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-10-06 22:29:29 +0100

    [MAP] - adding test for the computeIfAbsent

---
+++ src/test/java/newmethods/MapMethodsTest.java
@@ -0,0 +1,19 @@
+package newmethods;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MapMethodsTest {
+
+    @Test
+    void computeIfAbsent() {
+        Map<String, Integer> stringLength = new HashMap<>();
+        stringLength.put("Sasi", 4);
+        var actualValue = new MapMethods<String, Integer>().computeIfAbsent(stringLength, "Sasi", String::length);
+        assertEquals(4, actualValue);
+    }
+}
