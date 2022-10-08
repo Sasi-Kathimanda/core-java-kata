cd881c3404f3f2097984913d8bf1809b0e4ffbb3
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-10-08 20:30:17 +0100

    [MAP] - computeIf absents records no mapping when mappping function returns null

--- src/test/java/newmethods/MapMethodsTest.java
+++ src/test/java/newmethods/MapMethodsTest.java
@@ -1,11 +1,13 @@
 package newmethods;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class MapMethodsTest {
 
@@ -23,5 +25,13 @@ class MapMethodsTest {
         stringLength.put("Sasi", 10);
         var actualValue = new MapMethods<String, Integer>().computeIfAbsent(stringLength, "Kiran", String::length);
         assertEquals(5, actualValue); //here the mapping function is executed ONLY when the Key is not matched
+    }
+
+    @Test
+    void computeIfAbsentReturnsNoMappingWhenMappingFunctionReturnsNull() {
+        Map<String, Integer> stringLength = new HashMap<>();
+        var actualValue = new MapMethods<String, Integer>().computeIfAbsent(stringLength, "Sasi", s -> null);
+        assertEquals(null, actualValue); // returns null
+        assertNull(stringLength.get("Sasi")); // records no mapping
     }
 }
