463a421ce41827a55357c52fda3dd43e07d7f32c
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-10-13 11:09:28 +0100

    [PROGRAM] - passing test for which floor

--- src/main/java/programming/FloorFinder.java
+++ src/main/java/programming/FloorFinder.java
@@ -4,7 +4,7 @@ public class FloorFinder {
 
     public int findFloor(String input) {
         int counter = 0;
-         char[] chars = input.toCharArray();
+        char[] chars = input.toCharArray();
         for (char aChar : chars) {
             if (aChar == '(') {
                 counter++;
@@ -16,6 +16,18 @@ public class FloorFinder {
     }
 
     public int firstTimeWhichFloor(int firstTimeFloorNumber, String input) {
-        return toString().length();
+        int counter = 0;
+        char[] chars = input.toCharArray();
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == '(') {
+                counter++;
+            } else {
+                counter--;
+            }
+            if (counter == firstTimeFloorNumber) {
+                return i;
+            }
+        }
+        return 0;
     }
 }
