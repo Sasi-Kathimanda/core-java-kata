b6f0aaa60a30d08e17b22f1e13525ebd018ab6f3
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-10-14 22:53:51 +0100

    [BAU] - remove wildcar imports

--- src/test/java/programming/FloorFinderTest.java
+++ src/test/java/programming/FloorFinderTest.java
@@ -2,7 +2,8 @@ package programming;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class FloorFinderTest {
 
