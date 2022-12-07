bc9f00dcc628c88513b7ca0dbb05e5b96a92dbea
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-12-07 22:46:17 +0000

    [BAU] - formatting output for the filtering out nines

--- src/main/java/codebat/functional2/No9.java
+++ src/main/java/codebat/functional2/No9.java
@@ -6,16 +6,19 @@ import java.util.stream.Collectors;
 
 public class No9 {
     public static void main(String[] args) {
-        List<Integer> list1 = Arrays.asList(1,2,19);
-        List<Integer>  list2 = Arrays.asList(9,19,29,3);
-        List<Integer>  list3 = Arrays.asList(1,2,3);
+        List<Integer> list1 = Arrays.asList(2, 2, 19);
+        List<Integer> list2 = Arrays.asList(9, 19, 29, 3);
+        List<Integer> list3 = Arrays.asList(7, 2, 3);
         no9(list1);
         no9(list2);
         no9(list3);
     }
 
-    public static List<Integer> no9(List<Integer> nums) {
-        return  nums.stream().filter( e -> e >= 0).filter(e-> e%10 != 9).collect(Collectors.toList());
+    public static void no9(List<Integer> nums) {
+        System.out.println(nums.stream()
+                .filter(e -> e >= 0)
+                .filter(e -> e % 10 != 9)
+                .collect(Collectors.toList()));
     }
 
 
