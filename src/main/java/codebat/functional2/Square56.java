74d646d53755b89c8377e3ff9e415f95227f5d87
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-04-19 20:26:56 +0100

    [CHECKSTYLE] - fix whitespace after

--- src/main/java/codebat/functional2/Square56.java
+++ src/main/java/codebat/functional2/Square56.java
@@ -6,9 +6,9 @@ import java.util.stream.Collectors;
 
 public class Square56 {
     public static void main(String[] args) {
-        List<Integer> list1 = Arrays.asList(3,1,4);
-        List<Integer>  list2 = Arrays.asList(1);
-        List<Integer>  list3 = Arrays.asList(2);
+        List<Integer> list1 = Arrays.asList(3, 1, 4);
+        List<Integer> list2 = Arrays.asList(1);
+        List<Integer> list3 = Arrays.asList(2);
         square56(list1);
         square56(list2);
         square56(list3);
@@ -16,7 +16,7 @@ public class Square56 {
 
     public static List<Integer> square56(List<Integer> nums) {
         List<Integer> list = nums.stream().map(e -> (e * e) + 10).filter(e -> !(e % 10 == 5 || e % 10 == 6)).collect(
-            Collectors.toList());
+                Collectors.toList());
         System.out.println(list);
         return list;
     }
