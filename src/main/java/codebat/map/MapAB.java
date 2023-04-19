a197d64660769d02b8b5641255842babbea1051d
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-04-19 20:28:34 +0100

    [CHECKSTYLE] - fix whitespace after

--- src/main/java/codebat/map/MapAB.java
+++ src/main/java/codebat/map/MapAB.java
@@ -5,9 +5,16 @@ import java.util.Map;
 
 public class MapAB {
     public static void main(String[] args) {
-        Map<String,String> map1 = new HashMap<String,String>(){{ putIfAbsent("a","Hi");putIfAbsent("b","There"); }};
-        Map<String,String>  map2 = new HashMap<String,String>(){{ putIfAbsent("a","Hi"); }};
-        Map<String,String>  map3 = new HashMap<String,String>(){{ putIfAbsent("b","There"); }};
+        Map<String, String> map1 = new HashMap<String, String>() {{
+            putIfAbsent("a", "Hi");
+            putIfAbsent("b", "There");
+        }};
+        Map<String, String> map2 = new HashMap<String, String>() {{
+            putIfAbsent("a", "Hi");
+        }};
+        Map<String, String> map3 = new HashMap<String, String>() {{
+            putIfAbsent("b", "There");
+        }};
 
         System.out.println(map1.entrySet());
         mapAB(map1);
@@ -26,8 +33,8 @@ public class MapAB {
     }
 
     public static Map<String, String> mapAB(Map<String, String> map) {
-        if(map.containsKey("a") && map.containsKey("b")) {
-            map.put("ab",map.get("a")+map.get("b"));
+        if (map.containsKey("a") && map.containsKey("b")) {
+            map.put("ab", map.get("a") + map.get("b"));
         }
         return map;
     }
