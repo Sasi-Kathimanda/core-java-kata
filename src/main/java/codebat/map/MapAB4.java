59224d684737d115a50b2c0f48c6013df3073c7b
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-04-21 21:09:01 +0100

    [Checkstyle]- fix WhitespaceAfter and ParentPad

--- src/main/java/codebat/map/MapAB4.java
+++ src/main/java/codebat/map/MapAB4.java
@@ -5,9 +5,20 @@ import java.util.Map;
 
 public class MapAB4 {
     public static void main(String[] args) {
-        Map<String,String> map1 = new HashMap<String,String>(){{ putIfAbsent("a","aaa");putIfAbsent("b","bb");putIfAbsent( "c","cake"); }};
-        Map<String,String>  map2 = new HashMap<String,String>(){{ putIfAbsent("a","aa"); putIfAbsent("b", "bbb");putIfAbsent( "c","cake");}};
-        Map<String,String>  map3 = new HashMap<String,String>(){{ putIfAbsent("a","aa");putIfAbsent("b","bbb"); }};
+        Map<String, String> map1 = new HashMap<String, String>() {{
+            putIfAbsent("a", "aaa");
+            putIfAbsent("b", "bb");
+            putIfAbsent("c", "cake");
+        }};
+        Map<String, String> map2 = new HashMap<String, String>() {{
+            putIfAbsent("a", "aa");
+            putIfAbsent("b", "bbb");
+            putIfAbsent("c", "cake");
+        }};
+        Map<String, String> map3 = new HashMap<String, String>() {{
+            putIfAbsent("a", "aa");
+            putIfAbsent("b", "bbb");
+        }};
 
         System.out.println(map1.entrySet());
         mapAB4(map1);
@@ -25,12 +36,12 @@ public class MapAB4 {
     }
 
     public static Map<String, String> mapAB4(Map<String, String> map) {
-        if(( map.get("a") != null && map.get("b") != null) && (map.get("a").length()!= map.get("b").length())) {
-            String longerValue =  map.get("a").length()> map.get("b").length() ? map.get("a"):map.get("b");
-            map.put("c",longerValue);
-        } else if(( map.get("a") != null && map.get("b") != null) && (map.get("a").length() == map.get("b").length())) {
-            map.put("a","");
-            map.put("b","");
+        if ((map.get("a") != null && map.get("b") != null) && (map.get("a").length() != map.get("b").length())) {
+            String longerValue = map.get("a").length() > map.get("b").length() ? map.get("a") : map.get("b");
+            map.put("c", longerValue);
+        } else if ((map.get("a") != null && map.get("b") != null) && (map.get("a").length() == map.get("b").length())) {
+            map.put("a", "");
+            map.put("b", "");
         }
         return map;
     }
