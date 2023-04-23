888aabe9c624937fb39c690f937a5eb1bdb4eb54
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-04-23 20:17:57 +0100

    [Checkstyle]- fix whitespaceAfter

--- src/main/java/codebat/map/MapShare.java
+++ src/main/java/codebat/map/MapShare.java
@@ -6,8 +6,15 @@ import java.util.Map;
 public class MapShare {
 
     public static void main(String[] args) {
-        Map<String,String> map1 = new HashMap<String,String>(){{ putIfAbsent("a","aaa");putIfAbsent("b","bbb");putIfAbsent("c","ccc"); }};
-        Map<String,String> map2 = new HashMap<String,String>(){{ putIfAbsent("b","xyz");putIfAbsent("c","ccc"); }};
+        Map<String, String> map1 = new HashMap<String, String>() {{
+            putIfAbsent("a", "aaa");
+            putIfAbsent("b", "bbb");
+            putIfAbsent("c", "ccc");
+        }};
+        Map<String, String> map2 = new HashMap<String, String>() {{
+            putIfAbsent("b", "xyz");
+            putIfAbsent("c", "ccc");
+        }};
         System.out.println(map2.entrySet());
         mapShare(map2);
         System.out.println(map2.entrySet());
@@ -15,8 +22,8 @@ public class MapShare {
     }
 
     public static Map<String, String> mapShare(Map<String, String> map) {
-        if (map.containsKey("a") && map.get("a").length()> 0) {
-            map.put("b",map.get("a"));
+        if (map.containsKey("a") && map.get("a").length() > 0) {
+            map.put("b", map.get("a"));
         }
         map.remove("c");
         return map;
