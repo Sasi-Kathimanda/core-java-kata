62f73276a82cf70548ae1c6fad2fb9d2015899a1
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-04-24 20:27:31 +0100

    [Checkstyle] - Fix Whitespace after and whitespace around

--- src/main/java/codebat/map/Topping2.java
+++ src/main/java/codebat/map/Topping2.java
@@ -5,9 +5,18 @@ import java.util.Map;
 
 public class Topping2 {
     public static void main(String[] args) {
-        Map<String, String> map1 = new HashMap<String, String>() {{ putIfAbsent("ice cream", "cherry"); }};
-        Map<String, String> map2 = new HashMap<String, String>() {{putIfAbsent("spinach", "dirt");putIfAbsent("ice cream", "cherry"); }};
-        Map<String, String> map3 = new HashMap<String, String>() {{ putIfAbsent("yogurt", "salt"); }};
+        Map<String, String> map1 = new HashMap<String, String>() {{
+            putIfAbsent("ice cream", "cherry");
+        }};
+        Map<String, String> map2 = new HashMap<String, String>() {
+            {
+                putIfAbsent("spinach", "dirt");
+                putIfAbsent("ice cream", "cherry");
+            }
+        };
+        Map<String, String> map3 = new HashMap<String, String>() {{
+            putIfAbsent("yogurt", "salt");
+        }};
 
         System.out.println(map1.entrySet());
         topping2(map1);
@@ -23,12 +32,13 @@ public class Topping2 {
         topping2(map3);
         System.out.println(map3.entrySet());
     }
+
     public static Map<String, String> topping2(Map<String, String> map) {
-        if(map.containsKey("ice cream")) {
-            map.put("yogurt",map.get("ice cream"));
+        if (map.containsKey("ice cream")) {
+            map.put("yogurt", map.get("ice cream"));
         }
         if (map.containsKey("spinach")) {
-            map.put("spinach","nuts");
+            map.put("spinach", "nuts");
         }
         return map;
     }
