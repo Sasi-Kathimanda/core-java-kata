7f11e02881e9c337896d3235f9a76a96c59921c1
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-04-25 21:05:49 +0100

    [Checkstyle] - Fix Whitespace after and whitespace around

--- src/main/java/codebat/map/Topping3.java
+++ src/main/java/codebat/map/Topping3.java
@@ -5,9 +5,16 @@ import java.util.Map;
 
 public class Topping3 {
     public static void main(String[] args) {
-        Map<String,String> map1 = new HashMap<String,String>(){{ putIfAbsent("potato","ketchup"); }};
-        Map<String,String> map2 = new HashMap<String,String>(){{ putIfAbsent("potato","butter"); }};
-        Map<String,String> map3 = new HashMap<String,String>(){{ putIfAbsent("salad","oil");putIfAbsent("potato","ketchup");}};
+        Map<String, String> map1 = new HashMap<String, String>() {{
+            putIfAbsent("potato", "ketchup");
+        }};
+        Map<String, String> map2 = new HashMap<String, String>() {{
+            putIfAbsent("potato", "butter");
+        }};
+        Map<String, String> map3 = new HashMap<String, String>() {{
+            putIfAbsent("salad", "oil");
+            putIfAbsent("potato", "ketchup");
+        }};
 
         System.out.println(map1.entrySet());
         topping3(map1);
@@ -25,11 +32,11 @@ public class Topping3 {
     }
 
     public static Map<String, String> topping3(Map<String, String> map) {
-        if(map.containsKey("potato") && map.get("potato").length() > 0) {
-            map.put("fries",map.get("potato"));
+        if (map.containsKey("potato") && map.get("potato").length() > 0) {
+            map.put("fries", map.get("potato"));
         }
         if (map.containsKey("salad") && map.get("salad").length() > 0) {
-            map.put("spinach",map.get("salad"));
+            map.put("spinach", map.get("salad"));
         }
         return map;
     }
