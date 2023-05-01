40524a41b2ce906d5677d862108d9d727307e25c
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-05-01 21:24:46 +0100

    [Checksyle]- formatting for checkstyle

--- src/main/java/programming/challenges/FizzBuzzTest.java
+++ src/main/java/programming/challenges/FizzBuzzTest.java
@@ -2,23 +2,23 @@ package programming.challenges;
 
 public class FizzBuzzTest {
     public static void main(String[] args) {
-    printFizzBuzz(10);
-}
+        printFizzBuzz(10);
+    }
 
     public static void printFizzBuzz(int input) {
-        for(int i = 1; i <= input;i++) {
-            if( i%3 == 0 && i%5 == 0){
+        for (int i = 1; i <= input; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (i%3 == 0){
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            } else if(i%5 == 0) {
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
             } else {
                 System.out.println(i);
             }
         }
     }
-    }
+}
 /*
 1
 2
