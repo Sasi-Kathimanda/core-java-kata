f2c40c6243489e2b31bd7238b33f772de340ecf8
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-05-01 21:23:09 +0100

    [Checkstyle]- adding parent padding

--- src/main/java/codebat/string1/FirstHalf.java
+++ src/main/java/codebat/string1/FirstHalf.java
@@ -2,13 +2,13 @@ package codebat.string1;
 
 public class FirstHalf {
     public static void main(String[] args) {
-        System.out.println(firstHalf("WooHoo") );
-        System.out.println(firstHalf("HelloThere") );
-        System.out.println(firstHalf("abcdef") );
+        System.out.println(firstHalf("WooHoo"));
+        System.out.println(firstHalf("HelloThere"));
+        System.out.println(firstHalf("abcdef"));
     }
 
     public static String firstHalf(String str) {
-        return str.substring(0,str.length()/2);
+        return str.substring(0, str.length() / 2);
     }
 
 }
