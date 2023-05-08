cbf31e4ea7107204ba6525a10d8df9cff1fa6591
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-05-08 21:08:23 +0100

    [Checkstyle]- fix WhitespaceAround

--- src/main/java/method/reference/MethodReferencesDifferentOptions.java
+++ src/main/java/method/reference/MethodReferencesDifferentOptions.java
@@ -9,21 +9,21 @@ import java.util.List;
 public class MethodReferencesDifferentOptions {
     public static void main(String[] args) {
 
-        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
         System.out.println("**** parameter as an argument ****");
-       // rather numbers.forEach(e -> System.out.println(e));
+        // rather numbers.forEach(e -> System.out.println(e));
         numbers.forEach(System.out::println); // where println is INSTANCE method
 
         System.out.println("**** parameter as an argument to static method ****");
         numbers.stream()
-               //rather  .map(e -> String.valueOf(e))
+                //rather  .map(e -> String.valueOf(e))
                 .map(String::valueOf) //where valueOf is STATIC method
                 .forEach(System.out::println);
 
         System.out.println("**** parameter as a target ****");
         numbers.stream()
-               //rather .map(e -> e.valueOf(e))
+                //rather .map(e -> e.valueOf(e))
                 .map(Integer::valueOf)
                 .forEach(System.out::println);
 
@@ -31,16 +31,16 @@ public class MethodReferencesDifferentOptions {
         System.out.println(
 //        numbers.stream()
 //                .reduce(0, (total,e) -> Integer.sum(total,e))
-          numbers.stream()
-                .reduce(0,Integer::sum)
+                numbers.stream()
+                        .reduce(0, Integer::sum)
         );
 
         System.out.println("**** two parameters,one as target the other as an argument. ****");
         System.out.println(
-        numbers.stream()
-                .map(String::valueOf)
-               //rather .reduce("",(carry,str) -> carry.concat(str))
-                .reduce("",String::concat)
+                numbers.stream()
+                        .map(String::valueOf)
+                        //rather .reduce("",(carry,str) -> carry.concat(str))
+                        .reduce("", String::concat)
         );
 
 
