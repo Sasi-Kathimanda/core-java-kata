84adb0ac8d4b40ba0adb4ceb1daeaef4ff07285d
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-05-10 15:00:47 +0100

    [Checkstyle]- correct WhitespaceAround

--- src/main/java/hackerrank/JavaSubStringComparision.java
+++ src/main/java/hackerrank/JavaSubStringComparision.java
@@ -3,24 +3,24 @@ package hackerrank;
 public class JavaSubStringComparision {
     public static void main(String[] args) {
         //System.out.println.);
-        getSmallestAndLargest("SasiKiranRajaKathimanda",5);
+        getSmallestAndLargest("SasiKiranRajaKathimanda", 5);
 
     }
 
     public static void getSmallestAndLargest(String s, int k) {
         String smallest = "";
         String largest = "";
-        String firstElement = s.substring(0,k);
+        String firstElement = s.substring(0, k);
 
-        for (int i = 0; i< s.length() & k<= s.length(); i++,k++) {
+        for (int i = 0; i < s.length() & k <= s.length(); i++, k++) {
 
-            if(firstElement.compareTo(s.substring(i,k)) <= 0) {
-                largest = s.substring(i,k);
+            if (firstElement.compareTo(s.substring(i, k)) <= 0) {
+                largest = s.substring(i, k);
             } else {
-                smallest = s.substring(i,k);
+                smallest = s.substring(i, k);
             }
         }
-        System.out.println("smallest ="+smallest+" largest="+largest);
+        System.out.println("smallest =" + smallest + " largest=" + largest);
 
         //return smallest + "\n" + largest;
     }
