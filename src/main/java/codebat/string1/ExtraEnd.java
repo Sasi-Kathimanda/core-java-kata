2a43294d5b174ba02ea76df45c1d9ed277b1cdab
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-05-11 20:08:21 +0100

    [checkstyle]-fix WhitespaceAround

--- src/main/java/codebat/string1/ExtraEnd.java
+++ src/main/java/codebat/string1/ExtraEnd.java
@@ -6,9 +6,10 @@ public class ExtraEnd {
         System.out.println(extraEnd("ab"));
         System.out.println(extraEnd("Hi"));
     }
+
     public static String extraEnd(String str) {
-        String end = str.substring(str.length()-2);
-        return end+end+end;
+        String end = str.substring(str.length() - 2);
+        return end + end + end;
     }
 
 }
