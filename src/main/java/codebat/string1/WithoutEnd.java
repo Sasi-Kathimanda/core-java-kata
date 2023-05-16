a55c146b5ae4cca3d2f5a15b82bce3c7d8f64580
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-05-16 22:32:13 +0100

    [Checkstyle]- fix whitespace after

--- src/main/java/codebat/string1/WithoutEnd.java
+++ src/main/java/codebat/string1/WithoutEnd.java
@@ -8,7 +8,7 @@ public class WithoutEnd {
     }
 
     public static String withoutEnd(String str) {
-        return str.substring(1,str.length()-1);
+        return str.substring(1, str.length() - 1);
     }
 
 }
