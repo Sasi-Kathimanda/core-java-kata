64a47f27fc8098574cb36365e4b8991e1fab129f
Sasi Kathimanda <ksasikiranraja@gmail.com>
2023-05-17 21:22:58 +0100

    [Checkstyle]- fix LocalVar name

--- src/main/java/hackerrank/JavaStringsIntro.java
+++ src/main/java/hackerrank/JavaStringsIntro.java
@@ -6,10 +6,10 @@ public class JavaStringsIntro {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String A = sc.next();
-        String B = sc.next();
-        System.out.println(A.length() + B.length());
-        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
-        System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " " + B.substring(0, 1).toUpperCase() + B.substring(1));
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a.length() + b.length());
+        System.out.println(a.compareTo(b) > 0 ? "Yes" : "No");
+        System.out.println(a.substring(0, 1).toUpperCase() + a.substring(1) + " " + b.substring(0, 1).toUpperCase() + b.substring(1));
     }
 }
