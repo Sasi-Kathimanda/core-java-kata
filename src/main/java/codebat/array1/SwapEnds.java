ceda3f897fa0b6384ec81f3f28a3de2a49c448d5
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2024-11-07 20:38:45 +0000

    add swap logic and the test

--- src/main/java/codebat/array1/SwapEnds.java
+++ src/main/java/codebat/array1/SwapEnds.java
@@ -1,17 +1,12 @@
 package codebat.array1;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class SwapEnds {
-    public static void main(String[] args) {
-        int[] list1 = new int[]{1, 2, 3, 4};
-    }
 
-    public static int[] swapEnds(int[] nums) {
-        List<int[]> numsList = Arrays.asList(nums);
-//        Arrays.stream(nums).collect(Collections.swap(numsList,nums[0],nums[nums.length]));
-        return new int[]{};
+    public int[] swapEnds(int[] nums) {
+        int temp = nums[0];
+        nums[0] = nums[nums.length - 1];
+        nums[nums.length - 1] = temp;
+        return nums;
     }
 
 }
