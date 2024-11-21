3790335f6b19cbade6bb9653b16a4eaadd3cfb7e
Sasi Kathimanda <ksasikiranraja@gmail.com>
2024-11-21 21:15:00 +0000

    formatting

--- src/main/java/streams/StreamsCreation.java
+++ src/main/java/streams/StreamsCreation.java
@@ -7,20 +7,20 @@ import java.util.stream.Stream;
 
 public class StreamsCreation {
     public static void main(String[] args) {
-        
+
         // 1 st way
         List<String> words = Arrays.asList("namasthey", "anna", "manchiga", "vunnava");
         Stream<String> stream1 = words.stream();
         System.out.println(stream1.count());
-        
+
         //2nd way
-        Stream<String> stream2 = Stream.of("namasthey", "anna", "manchiga","vunnava");
+        Stream<String> stream2 = Stream.of("namasthey", "anna", "manchiga", "vunnava");
         System.out.println(stream2.count());
 
-        
+
         //3rd way
-        String[] words2 = {"namasthey", "anna", "manchiga","vunnava"};
-        Stream<String> stream3 =  Stream.of(words2);
+        String[] words2 = {"namasthey", "anna", "manchiga", "vunnava"};
+        Stream<String> stream3 = Stream.of(words2);
         System.out.println(stream3.count());
 
 
@@ -30,7 +30,6 @@ public class StreamsCreation {
 
         Integer[] nums2 = {1, 2, 3, 4, 5};
         System.out.println(Stream.of(nums2).count()); //
-
 
 
     }
