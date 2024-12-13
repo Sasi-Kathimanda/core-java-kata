92291de40e0d999c391c847f85b1d2841abc31ae
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2024-12-13 09:20:21 +0000

    [ENUM]-usage of enum in the switch statement

--- src/main/java/enums/FlightCrewJob.java
+++ src/main/java/enums/FlightCrewJob.java
@@ -3,5 +3,15 @@ package enums;
 public enum FlightCrewJob {
     FLIGHT_ATTENDANT,
     CO_PILOT,
-    PILOT
+    PILOT;
+
+    public static String getJobDescription(FlightCrewJob crewJob) {
+        var matchedJob = "";
+        switch (crewJob) {
+            case FLIGHT_ATTENDANT -> matchedJob = "FLIGHT_ATTENDANT serves the passengers" ;
+            case CO_PILOT -> matchedJob = "CO_PILOT serves the PILOT";
+            case PILOT -> matchedJob = "PILOT drives the plane";
+        }
+        return matchedJob;
+    }
 }
