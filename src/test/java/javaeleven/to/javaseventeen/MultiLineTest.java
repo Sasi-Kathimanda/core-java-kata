b63a01a86711a8100fca279f10f6148ea3280808
Sasi Kathimanda <ksasikiranraja@gmail.com>
2024-12-31 22:20:03 +0000

    [CHECKSTYLE]-fixing the warnings

--- src/test/java/javaeleven/to/javaseventeen/MultiLineTest.java
+++ src/test/java/javaeleven/to/javaseventeen/MultiLineTest.java
@@ -1,25 +1,24 @@
 package javaeleven.to.javaseventeen;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MultiLineTest {
     @Test
     void givenMultiLineStringJ17CouldProcessCorrectly() {
         MultiLine multiLine = new MultiLine();
-        String OldString = "{\n" +
+        String oldString = "{\n" +
                 " \"name\": \"Sasi K\",\n" +
                 " \"age\": 18,\n" +
                 "}\n";
-        String multilineString =
+        String multiLineString =
                  """
                  {
                   "name": "Sasi K",
                   "age": 18,
                  }
                  """;
-        multiLine.fromMultiLineToNormalisedString(multilineString);
-        assertEquals(OldString, multilineString);
+        multiLine.fromMultiLineToNormalisedString(multiLineString);
+        assertEquals(oldString, multiLineString);
     }
 }
