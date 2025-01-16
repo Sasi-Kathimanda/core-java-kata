209c6ee627ada529a45c763a304af899095406f9
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2025-01-16 21:02:46 +0000

    [CHECKSTYLE]-fix the warnings

--- src/test/java/hf/ooad/dessert/DessertCounterImpl.java
+++ src/test/java/hf/ooad/dessert/DessertCounterImpl.java
@@ -1,8 +1,5 @@
 package hf.ooad.dessert;
 
-import hf.ooad.dessert.icecream.IceCream;
-import hf.ooad.dessert.topping.Topping;
-
 public class DessertCounterImpl implements DessertCounter {
     private Dessert dessert;
     @Override
