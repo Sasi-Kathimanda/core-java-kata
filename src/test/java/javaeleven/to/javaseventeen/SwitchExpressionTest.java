609b108928e4e212073b0d7f3bf04366637171ee
Sasi Kathimanda <ksasikiranraja@gmail.com>
2025-01-18 22:37:32 +0000

    [CHECKSTYLE]-fixing the warnings

--- src/test/java/javaeleven/to/javaseventeen/SwitchExpressionTest.java
+++ src/test/java/javaeleven/to/javaseventeen/SwitchExpressionTest.java
@@ -1,8 +1,7 @@
 package javaeleven.to.javaseventeen;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SwitchExpressionTest {
 
