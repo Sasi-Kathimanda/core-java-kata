5a2306438b58926059a7d396d8759539d9298ccd
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2025-01-23 21:41:56 +0000

    [CHECKSTYLE]-fix the warnings

--- src/test/java/newmethods/CollectionMethodsTest.java
+++ src/test/java/newmethods/CollectionMethodsTest.java
@@ -2,10 +2,8 @@ package newmethods;
 
 import lambda.Person;
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CollectionMethodsTest {
