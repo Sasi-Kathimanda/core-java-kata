a27e3f9c565b700ba2a2ae6bef21d0ce654e64ed
Sasi Kathimanda <ksasikiranraja@gmail.com>
2025-01-26 21:32:29 +0000

    [CHECKSTYLE]-fixing the warnings

--- src/test/java/programming/challenges/FactorialTest.java
+++ src/test/java/programming/challenges/FactorialTest.java
@@ -1,7 +1,9 @@
 package programming.challenges;
 
 import org.junit.jupiter.api.Test;
+
 import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -9,10 +11,10 @@ class FactorialTest {
     private final BigInteger hundredFactorial = new BigInteger("93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000");
 
     @Test
-    void findFactorialForAVeryLargeNumber(){
+    void findFactorialForAVeryLargeNumber() {
         Factorial factorial = new Factorial();
         BigInteger actualResult = factorial.findFactorialForAVeryLargeNumber(100);
-        assertEquals(hundredFactorial.toString(),  actualResult.toString());
+        assertEquals(hundredFactorial.toString(), actualResult.toString());
     }
 
 }
